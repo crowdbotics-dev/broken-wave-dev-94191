a49aa44b1eb971407f1779bd8f2c69d03e448024
Crowdbotics <engineering@crowdbotics.com>
2023-06-23 10:03:07 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "broken-wave-dev-94191",
+  url: "https://broken-wave-dev-94191.botics.co",
+  api: "https://broken-wave-dev-94191.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
